665c3892200655e78860c4863bc2e3b08898908b
khanhpro1250 <51900114@student.tdtu.edu.vn>
2021-12-27 12:28:18 +0700

    add path login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const homeRouter = require('./homeRouter')
+const loginRouter = require('./loginRouter')
 
 function route(app) {
+
+    app.use('/login', loginRouter)
     app.use('/', homeRouter)
+
 }
 
 module.exports = route
