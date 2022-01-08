1bf8ba7a9a9363de3043612536a178d95937a4a6
khanhpro1250 <51900114@student.tdtu.edu.vn>
2022-01-08 21:15:28 +0700

    test login deloy

--- credentials.js
+++ credentials.js
@@ -3,7 +3,7 @@ module.exports = {
         gooogle: {
             development: {
                 appId: "182621371836-5geucbok4teva19lcfsiugvkcv35fvgb.apps.googleusercontent.com", 
-                appSecret: "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",
+                appSecret: "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",
                 callbackURL: '/account/google/callback',
             }
         }
