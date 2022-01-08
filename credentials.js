3e66f508f10d3b25aa56330bb9333e16346331d6
khanhpro1250 <51900114@student.tdtu.edu.vn>
2022-01-08 21:56:05 +0700

    test login gg

--- credentials.js
+++ credentials.js
@@ -4,7 +4,7 @@ module.exports = {
             development: {
                 appId: "182621371836-5geucbok4teva19lcfsiugvkcv35fvgb.apps.googleusercontent.com", 
                 appSecret: "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",
-                callbackURL: '/account/google/callback',
+                callbackURL: 'https://ga-con.up.railway.app/account/google/callback',
             }
         }
     }
