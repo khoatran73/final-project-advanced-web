e1900fece11d921a050d61ae6b192eba7332521f
KhoaHenry <trananhkhoa73@gmail.com>
2022-01-08 20:24:38 +0700

    account gg

--- credentials.js
+++ credentials.js
@@ -4,7 +4,7 @@ module.exports = {
             development: {
                 appId: "182621371836-5geucbok4teva19lcfsiugvkcv35fvgb.apps.googleusercontent.com", 
                 appSecret: "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",
-                callbackURL: 'http://localhost:3000/account/google/callback',
+                callbackURL: '/account/google/callback',
             }
         }
     }
